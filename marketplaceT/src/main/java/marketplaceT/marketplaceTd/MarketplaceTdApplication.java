727c88e339b818282280ad07977953a7a1368d32
PC <PC@192.168.1.11>
2021-09-27 15:42:37 -0500

    primer commit 1

---
+++ marketplaceT/src/main/java/marketplaceT/marketplaceTd/MarketplaceTdApplication.java
@@ -0,0 +1,13 @@
+package marketplaceT.marketplaceTd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceTdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceTdApplication.class, args);
+	}
+
+}
---
+++ marketplaceT/src/test/java/marketplaceT/marketplaceTd/MarketplaceTdApplicationTests.java
@@ -0,0 +1,13 @@
+package marketplaceT.marketplaceTd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceTdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
